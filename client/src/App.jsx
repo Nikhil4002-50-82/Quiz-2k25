8c7ccf549b608df9c8e80a2c9339748bee359b1d
Nikhil <nikhil.4002.50.82@gmail.com>
2025-06-19 00:36:26 +0530

    fixed some missing routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import StudentDashboard from "./components/StudentDashboard/StudentDashboard";
 import TeacherDashboard from "./components/TeacherDashboard/TeacherDashboard";
@@ -21,7 +21,7 @@ const App = () => {
       setUserData(user);
     }
   }, []);
-  
+
   return (
     <div className="font-edu">
       <LoginContext.Provider
@@ -30,8 +30,26 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<AuthComponent />} />
-            <Route path="/student" element={<StudentDashboard />} />
-            <Route path="/teacher" element={<TeacherDashboard />} />
+            <Route
+              path="/student"
+              element={
+                loggedIn && userData?.role === "student" ? (
+                  <StudentDashboard />
+                ) : (
+                  <Navigate to="/" replace />
+                )
+              }
+            />
+            <Route
+              path="/teacher"
+              element={
+                loggedIn && userData?.role === "teacher" ? (
+                  <TeacherDashboard />
+                ) : (
+                  <Navigate to="/" replace />
+                )
+              }
+            />
             <Route path="/newQuiz" element={<NewQuiz />} />
             <Route path="/attemptQuiz" element={<AttemptQuiz />} />
           </Routes>
